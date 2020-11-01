026619f80fe5928861cd82ebdd092132e8c4abbb
Aris DS <dropsuite@Dropsuites-MacBook-Pro.local>
2020-11-01 23:42:33 +0700

    Initial Commit

---
+++ src/main/java/tugas18/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas18;
+
+/**
+ *
+ * @author
+ * Aris Prabowo
+ * 10119914 - IF10k
+ */
+public class Main {
+    public static void main(String[] args) {
+       float Bilangan = 3.5f;
+       float MaximumBilangan = 35f;
+       for(var i = Bilangan; i<=MaximumBilangan; i+=Bilangan){
+           System.out.println(i);
+       }
+    }
+}
